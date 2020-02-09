277e6701bba25bbb22b5264a84f730cf4b112f8e
irene-haeryun-kwon <haeryun2004@gmail.com>
2020-02-09 17:28:14 -0500

    Added changeNavOnScroll function to main.js

--- js/main.js
+++ js/main.js
@@ -3,16 +3,32 @@
     General
 */
 $(document).ready(function() {
-    scrollDown();
+    scrollToVisit();
+    changeNavOnScroll();
 });
 
 /*
     Scroll Down
 */
-function scrollDown() {
+function scrollToVisit() {
     $('.learn-more').click(function() {
         $('html, body').animate({
             scrollTop: $('#visit').offset().top
         }, 150);
     });
+}
+
+/*
+    Navbar
+*/
+function changeNavOnScroll() {
+    var from = $('#visit').offset().top;
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > from) {
+            $('#navbar').css("background", "#000000");
+        } else {
+            $('#navbar').css("background", "none");
+        }
+    })
 }
