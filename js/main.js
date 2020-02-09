d6cf08b0d1b935ac6cd3a2edf0d29124c795fe8f
irene-haeryun-kwon <haeryun2004@gmail.com>
2020-02-09 17:06:23 -0500

    Added a slider

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@
 */
 $(document).ready(function() {
     scrollDown();
-    moveRight();
 });
 
 /*
@@ -15,18 +14,5 @@ function scrollDown() {
         $('html, body').animate({
             scrollTop: $('#visit').offset().top
         }, 150);
-    });
-}
-
-/*
-    Slider
-*/
-function moveRight() {
-    $('.fa-chevron-left').click(function(){
-        $(".active").removeClass('landing-2').addClass('landing-1');
-    });
-
-    $('.fa-chevron-right').click(function(){
-        $(".active").removeClass('landing-2').addClass('landing-3');
     });
 }
