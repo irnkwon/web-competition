4ba947b5844a55bdd4c3e03f6475ac061f31775d
irene-haeryun-kwon <haeryun2004@gmail.com>
2020-02-09 21:32:54 -0500

    Completed php modularization

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="images/logo.png">
+        <title>Vancouver Aquarium</title>
+        
+        <!-- Scripts -->
+        <script src="js/jquery.js"></script>
+        <script src="js/main.js"></script>
+        <script src="https://kit.fontawesome.com/46032a98a9.js" crossorigin="anonymous"></script>
+        
+        <!-- Styles -->
+        <link rel="stylesheet" href="styles/main.css">
+    </head>
+    <body>
+        <?php include './modules/navbar.inc' ?>
+        <?php include './modules/home.inc' ?>
+        <?php include './modules/visit.inc' ?>
+        <?php include './modules/explore.inc' ?>  
+        <?php include './modules/contact.inc' ?>
+        <?php include './modules/footer.inc' ?>
+    </body>
+</html>
